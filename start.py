f27e55cd81671d0355a84a91b4b1ae559e2b8b03
chenyanxiself <48513344+chenyanxiself@users.noreply.github.com>
2021-09-10 17:39:56 +0800

    Update start.py

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ from env_config.settings import get_settings
 import optparse
 
 if __name__ == '__main__':
-    usage = "python run.py -e/--environment <local|prod|dev> "
+    usage = "python start.py -e/--environment <local|prod|dev> "
     parser = optparse.OptionParser(usage)
     parser.add_option('-e', '--environment', dest='env', type='string', help='environment', default='local')
     options, args = parser.parse_args()
